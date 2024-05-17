15d95e26400af18fa6049dfd7f92a1fc784d43e3
Sergio Nicolas Berton <sergionberton@gmail.com>
2024-05-17 16:41:10 -0300

    portada

--- js/app.js
+++ js/app.js
@@ -46,3 +46,32 @@ botonActivar.addEventListener('click', function() {
 botonDesactivar.addEventListener('click', function() {
     elemento.classList.remove('activado');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const carouselInner = document.querySelector('.carousel-inner');
+    const items = document.querySelectorAll('.carousel-item');
+    const totalItems = items.length;
+    let currentIndex = 0;
+  
+    function showSlide(index) {
+      items.forEach((item, i) => {
+        item.classList.toggle('active', i === index);
+      });
+    }
+  
+    function nextSlide() {
+      currentIndex = (currentIndex + 1) % totalItems;
+      showSlide(currentIndex);
+    }
+  
+    function prevSlide() {
+      currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+      showSlide(currentIndex);
+    }
+  
+    // Inicializar el primer slide
+    showSlide(currentIndex);
+  
+    // Automáticamente cambiar de slide cada 1 segundo
+    setInterval(nextSlide, 5000);
+  });
