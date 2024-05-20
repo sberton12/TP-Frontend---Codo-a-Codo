494095a695aebf4e4815af1c307cd08352a406cb
Sergio Nicolas Berton <sergionberton@gmail.com>
2024-05-19 21:08:41 -0300

    for

--- js/contactApp.js
+++ js/contactApp.js
@@ -3,60 +3,57 @@ const ERROR_COLOR = "red";
 
 //Formulario de Contactos
 const FormContact = document.getElementById("Form");
-FormContact.onsubmit=validacionFormulario();
+FormContact.onsubmit = validacionFormulario();
 
 //Validacion de campos obligatorios
-function validacionFormulario (){
-  var error=0;
+function validacionFormulario() {
+  var error = 0;
 
   var elementNombre = document.getElementById("nombreID");
-  if(!validarNombreYApellido(elementNombre.value)){
-    changeBorderColor (ERROR_COLOR,elementNombre)
-    error=1
+  if (!validarNombreYApellido(elementNombre.value)) {
+    changeBorderColor(ERROR_COLOR, elementNombre)
+    error = 1
   }
   var elementApellido = document.getElementById("apellidoID");
-  if(!validarNombreYApellido(elementApellido.value)){
-    changeBorderColor (ERROR_COLOR,elementApellido)
-    error=1
+  if (!validarNombreYApellido(elementApellido.value)) {
+    changeBorderColor(ERROR_COLOR, elementApellido)
+    error = 1
   }
   var elementEmail = document.getElementById("telefonoID");
-  if(!validarTelefono(elementEmail.value)){
-    changeBorderColor (ERROR_COLOR,elementEmail)
-    error=1
+  if (!validarTelefono(elementEmail.value)) {
+    changeBorderColor(ERROR_COLOR, elementEmail)
+    error = 1
   }
   var elementCel = document.getElementById("emailID");
-  if(!validarEmail(elementCel.value)){
-    changeBorderColor (ERROR_COLOR,elementCel)
-    error=1
+  if (!validarEmail(elementCel.value)) {
+    changeBorderColor(ERROR_COLOR, elementCel)
+    error = 1
   }
   var email = document.getElementById("emailID").value;
   var telefonoID = document.getElementById("telefonoID").value;
 
-  if (nombre==""||apellido==""||email==""||telefonoID==""){   
-      alert ("Completa todos los campos obligatorios")
-      return false
+  if (error) {
+    alert("Completa todos los campos obligatorios")
+    return false
+  } else {
+    alert("formulario enviado")
+    return true
   }
-  if (!validarEmail(email)) {
-      alert ("Correo no valido")
-      return false
-  }
-  alert ("formulario enviado")
-  return true
 }
-  
+
 //No contiene numeros ni caracteres especiales
-function validarNombreYApellido(text){
+function validarNombreYApellido(text) {
   const regex = /^[A-Za-z\s]+$/;
   return regex.test(text);
 }
 //No contiene numeros ni caracteres especiales
-function validarTelefono(num){
+function validarTelefono(num) {
   const regex = /^[0-9]+$/;
   return regex.test(num);
 }
 
 function validarEmail(email) {
-const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   return regex.test(email);
 }
 
@@ -69,19 +66,19 @@ const elemento = document.querySelector('.nav-movile-popup');
 const botonActivarMenu = document.getElementById('icon-menu');
 const botonDesactivarMenu = document.getElementById('faxx2');
 
-botonActivarMenu.addEventListener('click', function() {
+botonActivarMenu.addEventListener('click', function () {
   elemento.classList.add('activado');
 });
-botonDesactivarMenu.addEventListener('click', function() {
+botonDesactivarMenu.addEventListener('click', function () {
   elemento.classList.remove('activado');
 });
 
 
-botonDesactivar.addEventListener('click', function() {
+botonDesactivar.addEventListener('click', function () {
   elemento.classList.remove('activado');
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const carouselInner = document.querySelector('.carousel-inner');
   const items = document.querySelectorAll('.carousel-item');
   const totalItems = items.length;
